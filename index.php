d72ac045fc0e0e3d49c225420b1e0b26f431763a
leanne <zeph@khtdr.gateway.sasktel.net>
2020-09-15 21:54:29 -0600

    added html and css for index page, added fonts

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V1</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="helper.css">
+	<link rel="stylesheet" type="text/css" href="stylesheet.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+				<div class="login100-pic js-tilt" data-tilt>
+					<img src="vanbt21.png" alt="IMG">
+				</div>
+
+				<form class="login100-form validate-form">
+					<span class="login100-form-title">
+						Member Login
+					</span>
+
+					<div class="wrap-input100">
+						<input class="input100" type="text" name="email" placeholder="Email">
+					</div>
+
+					<div class="wrap-input100">
+						<input class="input100" type="password" name="pass" placeholder="Password">
+					</div>
+					
+					<div class="container-login100-form-btn">
+						<button class="login100-form-btn">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center p-t-136">
+						<a class="txt1">
+							Created by Devonian Software Development Ltd.
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+
+</body>
+</html>
