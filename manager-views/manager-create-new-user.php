36cdc9cac7d23f8cfd271e2e10e1666660070b7f
Teclado-Gif <nathan.e.slaney@gmail.com>
2020-11-13 21:31:11 -0600

    changed path to actually go to the javascript

--- manager-views/manager-create-new-user.php
+++ manager-views/manager-create-new-user.php
@@ -198,7 +198,7 @@
 <html lang="en">
 <head>
     <title>Create New User</title>
-    <script type="text/javascript" src="javascript/validationFunctions.js"></script>
+    <script type="text/javascript" src="../javascript/validationFunctions.js"></script>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
@@ -335,5 +335,5 @@
 	</div>
 
 </body>
-<script type="text/javascript" src="javascript/validationFunctionsR.js"></script>
+<script type="text/javascript" src="../javascript/validationFunctionsR.js"></script>
 </html>
