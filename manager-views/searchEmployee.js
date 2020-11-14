ea18fde31caf838fa75c692154d4c01bacb8b46e
Ryan Scott <Ryan@Ryans-MacBook-Pro.local>
2020-11-13 22:54:00 -0600

    Removed console debugging messages

--- manager-views/searchEmployee.js
+++ manager-views/searchEmployee.js
@@ -1,34 +1,23 @@
-console.log("AJAX Script LOCKED AND LOADED");
-
 //getEmployee AJAX function
 function getEmployee (event) {
     var searchVal = event.currentTarget.value;
-    console.log("IN THE SEARCH BAR: " + searchVal + "");
 
     //Create an instance of an XMLHttpRequest object
     var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4 && xhr.status == 200) {
-            console.log("       CALLBACK FUNCTION EXECUTING");
-
             //Parse the JSON object
             var newEmployees = JSON.parse(xhr.responseText);
 
             //Update the DOM here
             var oldTables = document.getElementsByName("employeeCard");
 
-            console.log("I'm about to run a for loop on you bitches");
-            console.log("This loop will run: "+ oldTables.length +" times.");
-
             var loopCounter = oldTables.length;
 
             //Delete all of the employees off of the page so we can insert the new ones
             for (var i = 0; i < loopCounter; i++)
-            {
-                console.log("DELETING NODE: " + i + "");
                 oldTables[0].remove();
-            }
 
             //Get the insertion point for the cells
             var parent = document.getElementById("searchEmployees");
@@ -46,15 +35,9 @@ function getEmployee (event) {
 
                     //Place all the information into variables
                     var FN = newEmployees[j].FirstName;
-                    console.log("FirstName: " + FN + "");
                     var LN = newEmployees[j].LastName;
-                    console.log("LastName: " + LN + "");
                     var BIO = newEmployees[j].ProfileBio;
-                    console.log("Profile Bio: " + BIO + "");
                     var EMAIL = newEmployees[j].Email;
-                    console.log("Email: " + EMAIL + "");
-
-                    console.log("MID: " + newEmployees[j].managerId + "");
 
                     if (newEmployees[j].managerId != null)
                         var ET = "Manager";
@@ -136,6 +119,5 @@ function getEmployee (event) {
 
 //Add event listener on the search bar
 var test = document.getElementById("employeeSearch");
-console.log("TAG: " + test + "");
 
 document.getElementById("employeeSearch").addEventListener("keyup", getEmployee);
