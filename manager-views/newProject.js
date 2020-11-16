644df69fb8cb0b05cfeba2b0a94dba3dbcea3f0d
Ryan Scott <Ryan@Ryans-MBP.gateway.sasktel.net>
2020-11-15 22:49:52 -0600

    All AJAX/PHP Functionality Working, Up on Hercules

--- manager-views/newProject.js
+++ manager-views/newProject.js
@@ -1,14 +1,20 @@
-console.log("AJAX LOCKED AND LOADED!");
 //Add to field function
 function addToField1 (event) {
     //Find the input field
     var inputField1 = document.getElementsByName("projectMember1");
 
     //Get the value of what is inside the p tag
-    var name1 = event.currentTarget.value;
+    var name1 = event.currentTarget.innerHTML;
 
     //Insert this value into the input field
-    inputField1[0].setAttribute("value", name1);
+    inputField1[0].value = name1;
+
+    //Delete all the created buttons
+    var buttons = document.getElementsByClassName("add-member-AJAX1");
+    var bLen = buttons.length;
+
+    for (var j = 0; j < bLen; j++)
+        buttons[0].remove(); 
 }
 
 function addToField2 (event) {
@@ -16,10 +22,17 @@ function addToField2 (event) {
     var inputField2 = document.getElementsByName("projectMember2");
 
     //Get the value of what is inside the p tag
-    var name2 = event.currentTarget.value;
+    var name2 = event.currentTarget.innerHTML;
 
     //Insert this value into the input field
-    inputField2[0].setAttribute("value", name2);
+    inputField2[0].value = name2;
+
+    //Delete all the created buttons
+    var buttons = document.getElementsByClassName("add-member-AJAX2");
+    var bLen = buttons.length;
+
+    for (var j = 0; j < bLen; j++)
+        buttons[0].remove();
 }
 
 function addToField3 (event) {
@@ -27,10 +40,17 @@ function addToField3 (event) {
     var inputField3 = document.getElementsByName("projectMember3");
 
     //Get the value of what is inside the p tag
-    var name3 = event.currentTarget.value;
+    var name3 = event.currentTarget.innerHTML;
 
     //Insert this value into the input field
-    inputField3[0].setAttribute("value", name3);
+    inputField3[0].value = name3;
+
+    //Delete all the created buttons
+    var buttons = document.getElementsByClassName("add-member-AJAX3");
+    var bLen = buttons.length;
+
+    for (var j = 0; j < bLen; j++)
+        buttons[0].remove();
 }
 
 function addToField4 (event) {
@@ -38,15 +58,21 @@ function addToField4 (event) {
     var inputField4 = document.getElementsByName("projectMember4");
 
     //Get the value of what is inside the p tag
-    var name4 = event.currentTarget.value;
+    var name4 = event.currentTarget.innerHTML;
 
     //Insert this value into the input field
-    inputField4[0].setAttribute("value", name4);
+    inputField4[0].value = name4;
+
+    //Delete all the created buttons
+    var buttons = document.getElementsByClassName("add-member-AJAX4");
+    var bLen = buttons.length;
+
+    for (var j = 0; j < bLen; j++)
+        buttons[0].remove();
 }
 
 //AJAX Function starts here
 function getMember (value, memNum) {
-    console.log("CHECKING VALUE: "+ value +"");
     //Create a new instance of an XMLHttpRequest Object
     var xhr = new XMLHttpRequest();
 
@@ -57,7 +83,6 @@ function getMember (value, memNum) {
 
             //Find the suggest member spot in the DOM
             var insertSpot = document.getElementById("suggestMember"+ memNum + "");
-            console.log("     INSERT SPOT:"+ insertSpot +"");
 
             //Find any old searches and remove them
             var oldSearches = document.getElementsByClassName("add-member-AJAX"+ memNum +"");
@@ -73,25 +98,20 @@ function getMember (value, memNum) {
                 //Insert the value into this spot
                 var node = document.createElement("p");
                 node.classList.add("add-member-AJAX"+ memNum +"");
-                node.setAttribute("id", "possibleMember" + memNum + "");
 
                 //Register an event on this item to insert into the field when clicked
                 switch (memNum) {
                     case 1: 
                         node.addEventListener("click", addToField1);
-                        console.log("Adding Field1 Event Listener");
                         break;
                     case 2: 
                         node.addEventListener("click", addToField2);
-                        console.log("Adding Field2 Event Listener");
                         break;
                     case 3: 
                         node.addEventListener("click", addToField3);
-                        console.log("Adding Field3 Event Listener");
                         break;
                     case 4:
                         node.addEventListener("click", addToField4);
-                        console.log("Adding Field4 Event Listener");
                 }
 
                 //Append the tag
@@ -109,28 +129,24 @@ function getMember (value, memNum) {
 function getMem1 (event) { 
     //Send to generic function with field input and ##
     var fieldInput1 = event.currentTarget.value;
-    console.log("Project Member 1: "+ fieldInput1 +"");
     getMember(fieldInput1, 1);
 }
 
 function getMem2 (event) {
     //Send to generic function with field input and ##
     var fieldInput2 = event.currentTarget.value;
-    console.log("Project Member 2: "+ fieldInput2 +"");
     getMember(fieldInput2, 2);
 }
 
 function getMem3 (event) {
     //Send to generic function with field input and ##
     var fieldInput3 = event.currentTarget.value;
-    console.log("Project Member 3: "+ fieldInput3 +"");
     getMember(fieldInput3, 3);
 }
 
 function getMem4 (event) {
     //Send to generic function with field input and ##
     var fieldInput4 = event.currentTarget.value;
-    console.log("Project Member 4: "+ fieldInput4 +"");
     getMember(fieldInput4, 4);
 }
 
